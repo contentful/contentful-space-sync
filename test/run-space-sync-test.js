bd3d5737e74d1ebbede8bb7c8b5806f29ebc9c3c
Tiago Rodrigues <tmcrodrigues@gmail.com>
2015-11-03 19:49:59 +0100

    Setup a test runner

---
+++ test/run-space-sync-test.js
@@ -0,0 +1,13 @@
+import test from 'tape'
+import runSpaceSync from '../lib/run-space-sync'
+
+test('Runs space sync', t => {
+  t.plan(1)
+  runSpaceSync()
+  .then(response => {
+    console.log(response)
+    t.ok(response)
+  }, err => {
+    t.end(err)
+  })
+})
