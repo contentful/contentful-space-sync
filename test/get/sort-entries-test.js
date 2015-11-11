db4502ead3f300633c178d673f467aaa182770ad
Tiago Rodrigues <tmcrodrigues@gmail.com>
2015-11-11 18:19:46 +0100

    Test entry sorting

---
+++ test/get/sort-entries-test.js
@@ -0,0 +1,31 @@
+import test from 'blue-tape'
+
+import sortEntries from '../../lib/get/sort-entries'
+
+const entries = [
+  {
+    sys: {id: '456'},
+    fields: {}
+  },
+  {
+    sys: {id: '123'},
+    fields: {
+      links: [
+        {
+          sys: {
+            type: 'Link',
+            linkType: 'Entry',
+            id: '456'
+          }
+        }
+      ]
+    }
+  }
+]
+
+test('Sorts entries by link order', t => {
+  const sortedEntries = sortEntries(entries)
+  t.equals(sortedEntries[0].sys.id, '123')
+  t.equals(sortedEntries[1].sys.id, '456')
+  t.end()
+})
