5b3afb790bb1594fd4cc2e7012b9a28db7e6558c
Tiago Rodrigues <tmcrodrigues@gmail.com>
2015-11-12 15:34:52 +0100

    test(push): deletion

---
+++ test/push/deletion-test.js
@@ -0,0 +1,25 @@
+import test from 'blue-tape'
+import sinon from 'sinon'
+import Promise from 'bluebird'
+
+import * as deletion from '../../lib/push/deletion'
+
+const logMock = {
+  info: sinon.stub()
+}
+deletion.__Rewire__('log', logMock)
+
+test('Delete entities', t => {
+  logMock.info.reset()
+  const space = {
+    deleteAsset: sinon.stub().returns(Promise.resolve())
+  }
+  return deletion.deleteEntities(space, [
+    { sys: {id: '123'} },
+    { sys: {id: '456'} }
+  ], 'Asset')
+  .then(response => {
+    t.equals(space.deleteAsset.callCount, 2, 'delete assets')
+    t.equals(logMock.info.callCount, 2, 'logs deletion of two assets')
+  })
+})
